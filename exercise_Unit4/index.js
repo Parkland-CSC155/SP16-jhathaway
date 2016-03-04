7f80d4905c0d3d88b2aa064cbd99a3b080fd3568
wombats17 <wombats17@gmail.com>
2016-03-04 17:30:17 -0600

    finished unit5 tried a few things with unit 4

--- exercise_Unit4/index.js
+++ exercise_Unit4/index.js
@@ -20,6 +20,7 @@ if(process.argv.indexOf("-output") > -1)
     var outputPath = path.resolve(folderArg2);
     
     //to see if output is there if not it creates it
+    /*/
     function mkdirIfNotExist(path){
         try{
             return fs.mkdirsync(path);
@@ -29,15 +30,28 @@ if(process.argv.indexOf("-output") > -1)
             
         }
         
-    }
+    }    
     
     
     mkdirIfNotExist(outputPath);
-    
+        /*/
+        
     
 }
 
-if(process.argv.indexOf("-folder") > -1 && process.argv.indexOf("-output") > -1)
+var dataPath = fs.readdirSync(folderPath);
+
+for (var i in dataPath)
 {
+var readingthefile = fs.readFileSync(dataPath[i]); 
+function writetothefile(readingthefile); 
+
     
+}
+
+
+//function to write to the text file
+function writetothefile(data){
+    
+    fs.appendFileSync(outputPath, data, 'utf8');      
 }
--- unit5assignment/index.js
+++ unit5assignment/index.js
@@ -1,11 +1,12 @@
 var fs = require("fs");
 var path = require("path");
-var verbosenum = 0;
+var log4js = require('log4js');
+var verbose = false;
 
 //verbose
 if(process.argv.indexOf("-v") > -1)
 {
-    var verbosenum = 1;
+    var verbose = true;
     //looking for the verbose switch
 }
 
@@ -32,11 +33,36 @@ if(process.argv.indexOf("-output") > -1)
     
 }
 
+//create logger
+
+log4js.configure({
+    appenders: [
+        { type: 'file', filename: './log.txt', category: 'file-logger' }
+    ]
+});
+
+var logger = log4js.getLogger('file-logger');
+
+if(verbose){
+    logger.setLevel("TRACE");
+}else{
+    logger.setLevel("INFO");
+}
+
+logger.trace("some trace message");
+logger.debug("some debug message");
+logger.info("some info message");
+logger.warn("some warn message");
+logger.error("some error message");
+
 //function to write to the text file
 function writetothefile(data){
     
-    fs.appendFile(_outputfile, data, 'utf8');      
+    fs.appendFileSync(_outputfile, data, 'utf8');      
 }
+
+
+
 
 //getting the list of files in the input directory
 var dataPath = fs.readdirSync(folderPath);
@@ -47,6 +73,9 @@ var dataPath = fs.readdirSync(folderPath);
 
 
 
+writetothefile("*** Overall Statistics ***" + '\r\n');
+writetothefile("CONTRACT | HIGH | LOW | AVERAGE SETTLE | TOTAL TRADING DAYS");
+writetothefile('\r\n');
 
 for (var i in dataPath)
 {
@@ -55,30 +84,10 @@ for (var i in dataPath)
     {
        /*/ 
     var filepath = path.resolve(folderPath, dataPath[i]);
-    var readingthefile = fs.readFile(filepath, function(err, data){
-        if (err)
-        {
-            return console.log(err);
-        }
-        console.log(data);
-    });
-    console.log(readingthefile);
-}
+    var readingthefile = fs.readFileSync(filepath);
+    
+    //reading each file in the dir
 
-writetothefile("*** Overall Statistics ***" + '\r\n');
-if(verbosenum === 1)
-{
-    writetothefile("CONTRACT | HIGH | LOW | AVERAGE | TOTAL | TRADING DAYS | NAME | ID | DATABASE_CODE" + '\r\n');      
-}
-
-else
-{
-    writetothefile("CONTRACT | HIGH | LOW | AVERAGE | TOTAL | TRADING DAYS" + '\r\n');
-}
-
-
-for (var i in dataPath)
-{
     /*/
     var filepath = path.resolve(folderPath, dataPath[i]);
 
@@ -91,57 +100,60 @@ for (var i in dataPath)
 //work right here to get the parsing to work and to find the object you need to send to the correct function
     var contents = fs.readFileSync(path.resolve(folderPath, dataPath[i]));
     var thedata = JSON.parse(contents);
-    writetothefile(thedata.dataset.dataset_code + " | ");
-    for (var a in thedata.dataset.data)
-    {
-        writetothefile(a + '\r\n' + '\r\n');
-    }
- 
+    writetothefile('\r\n');
     
-    /*/
-    high(filepath);
-    low(filepath);
-    average(filepath);
-    total(filepath);
-    /*/    
+    writetothefile(thedata.dataset.dataset_code);
+    writetothefile(' | ');
     
-    //add the more data here. The verbose switch is turned on
-    if(verbosenum === 1)
+    //high
+    var count = 0;
+    var num = 0;
+    
+    for (var hi in thedata.dataset.data)
     {
-        writetothefile(thedata.dataset.name + " | ");
-        writetothefile(thedata.dataset.id + " | ");
-        writetothefile(thedata.dataset.database_code + " | ");
+        if (num < thedata.dataset.data[count][2]) {
+            num = thedata.dataset.data[count][2];
+        }
+        count++;     
+     
     }
+    writetothefile(num);
+    writetothefile(' | ');
+    
+    //low
+    
+    var highnum = 1000
+    var lowcount = 0;
+    for (var sup in thedata.dataset.data)
+    {
+            if (highnum > thedata.dataset.data[lowcount][3] && 0 < thedata.dataset.data[lowcount][3]) {
+                highnum = thedata.dataset.data[lowcount][3];
+            }
+        
+        lowcount++;     
+     
+    }
+    writetothefile(highnum);
+    writetothefile(' | ');
+    
+    var avnum = 0;
+    var avcount = 0;
+    for (var sup in thedata.dataset.data)
+    {
+        avnum = thedata.dataset.data[avcount][6] + avnum;
+        avcount++;
+    }
+    var averagenumvar = avnum / avcount;
+    writetothefile(averagenumvar);
+    writetothefile(' | ');
+    
+    var daycount = 0;
+    for (var days in thedata.dataset.data)
+    {
+        daycount++;
+    }
+    writetothefile(daycount);
     
     writetothefile('\r\n')
 }
-
-
-
-
-
-
-function high(path){
-    var num = 0;
-    for (var i in path)
-    {
-        
-    }
-    
-    writetothefile()
-}
-
-function low(path){
-    
-    writetothefile()
-}
-
-function average(path){
-    
-    writetothefile()
-}
-
-function total(path){
-    
-    writetothefile()
-}
+//right file
