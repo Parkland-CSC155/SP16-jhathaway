49fd582bb6a829ad86dcbb8710f8f9d1be8477c5
wombats17 <wombats17@gmail.com>
2016-04-01 23:28:35 -0500

    Unit11

--- unit11exercise/index.js
+++ unit11exercise/index.js
@@ -2,7 +2,7 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('C:/Temp/chinook/chinook.db');
 var path = require("path");
-var jsonfile = require('jsonfile')
+var fs = require('fs');
 
 if(process.argv.indexOf("-output") > -1)
 {
@@ -14,15 +14,39 @@ if(process.argv.indexOf("-output") > -1)
     //console.log(folderPath);
     //console.log("input");
 }
+var file1 = path.join(_outputfile, 'customers_usa.json');
+var file2 = path.join(_outputfile, 'invoices_2013.json');
 
-var file1 = path.join('_outputfile', 'customers_usa.json');
-var obj = {name: 'JP'}
+var sql = `
+SELECT * 
+FROM customers
+WHERE Country = ?
+`;
  
-jsonfile.writeFile(file1, obj, function (err) {
-  console.error(err)
-})
+db.all(sql, 'USA', function(err, rows)
+{
+    console.error(err);
+    var json = JSON.stringify({"USA": rows});
+    fs.writeFile(file1, json, function (err1) 
+    {
+        console.error(err1);
+    })
+});
 
-var file2 = path.join('_outputfile', 'invoices_2013.json');
 
+var sql2 = `
+SELECT * 
+FROM invoices
+WHERE InvoiceDate BETWEEN '2013-01-01' AND '2013-12-31'
+`;
 
-
+db.all(sql2, function(err2, rows2)
+{
+    console.error(err2);
+    console.log(rows2);
+    var json2 = JSON.stringify(rows2);
+    fs.writeFile(file2, json2, function (err3) 
+    {
+        console.error(err3);
+    })
+});
